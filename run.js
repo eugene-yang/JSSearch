7d9b778ac1045cd879189f596502f1b63113c5c3
Eugene Yang <eugene2528@gmail.com>
2016-09-12 20:36:07 -0400

    Add special charactors replacement

--- run.js
+++ run.js
@@ -1,6 +1,7 @@
 var fs = require('fs')
 var cheerio = require('cheerio')
 var JSSU = require('./JSS/ir_utilities.js')
+var JSSConst = require('./JSS/constants.js')
 
 var log = console.log.bind(console);
 
@@ -12,14 +13,17 @@ var fn = "fr940104.2";
 //fs.readdirSync(fileDir).forEach(function(fn){
 	fs.readFile( fileDir + fn, 'utf8', function(err,data){
 		// remove special chars
-		data = data.replace(/&blank;/g, "");
-		data = data.replace(/&nbsp;|&alt;/g, " ");
+		JSSConst.SpecialChars.forEach(function(pair){
+			data = data.replace(pair[0], pair[1]);
+		})
+
+
 		var $ = cheerio.load(data);
 
 		$('DOC').eq(0).each(function(){
 			var no = $(this).find('DOCNO').text(),
 				text = new JSSU.String( $(this).find('TEXT').text() );
-			log( text.text );
+			//log( text.text );
 			text.token;
 		})
 	} )
