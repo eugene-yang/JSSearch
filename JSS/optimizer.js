6eadbe2ce38246ee75597f93e5fc8639fa9c2f1f
Eugene Yang <eugene2528@gmail.com>
2016-12-04 02:41:22 -0500

    n-ary search in optimizer

--- JSS/optimizer.js
+++ JSS/optimizer.js
@@ -17,19 +17,48 @@
 		return JSON.parse( JSON.stringify(this) )
 	}
 
-	function* iterParams(paramKeys, params) {
+	var chunkHyperParam = 5;
+
+	function* iterParams(paramKeys, params, feedback) {
 		if( paramKeys.length == 0 )
 			yield params;
 		else { 
 			var cloneKeys = paramKeys.clone(),
 				currentKey = cloneKeys.pop();
-			// if( typeof(params[currentKey][Symbol.iterator]) !== 'function' )
-			// 	params[currentKey] = [ params[currentKey] ]
-			for( var num of params[currentKey] ){
-				var deep = params.clone();
-				deep[currentKey] = num;
-				yield* iterParams(cloneKeys, deep);
-			}
+			if( typeof(params[currentKey][Symbol.iterator]) !== 'function' )
+				params[currentKey] = [ params[currentKey] ]
+
+			var ran = [];
+			var gloOpt = null
+			var i=0
+			var j=params[currentKey].length - 1;
+			do{
+				// chunk i ~ j
+				var optInd = null
+				var optVal = null
+				var step = Math.max( (j-i)/(chunkHyperParam-1), 1);
+				
+				for( var k=i; k<=j; k+=step ){
+					var ind = Math.round(k)
+					if( !!ran[ind] )
+						continue;
+					ran[ind] = true
+
+					var deep = params.clone();
+					deep[currentKey] = params[currentKey][ind];
+					yield* iterParams(cloneKeys, deep, feedback);
+					var val = feedback.value;
+					if( optVal == null || ( feedback.opt == "max" && optVal < val ) || ( feedback.opt == "min" && optVal > val ) ){
+						optVal = val;
+						optInd = ind
+					}
+				}
+				// update i, j
+				i = 0 >= optInd - Math.round(step) ? 0 : optInd - Math.round(step);
+				j = params[currentKey].length - 1 <= optInd + Math.round(step) ? params[currentKey].length - 1 : optInd + Math.round(step);
+				console.log(currentKey, i, params[currentKey][i], j, params[currentKey][j], Math.round(step))
+
+			} while( i < j-1 )
 		}
 	}
 
@@ -38,8 +67,10 @@
 		var opt = typeof(opt) === 'undefined' ? "max" : opt;
 		var record = typeof(record) === 'undefined' ? false : record;
 		var optValue = null, optParam = null, results = [];
-		for( let paramSet of iterParams(Object.keys(params), params) ){
+		var feedback = { value: null, opt: opt }
+		for( let paramSet of iterParams(Object.keys(params), params, feedback) ){
 			var val = f(paramSet);
+			feedback.value = val;
 			console.log( paramSet, val )
 
 			if( optValue == null || ( opt == "max" && optValue < val ) || ( opt == "min" && optValue > val ) ){
